028a2c98ff1f0ab08bc1efe79bf741b8b02337a1
nigh7sh4de <plotnikd@gmail.com>
2016-02-14 00:54:48 -0500

    Make watchify verbose in start script

--- flux/start_watchify.bat
+++ flux/start_watchify.bat
@@ -1,6 +1,6 @@
 start nodemon server.js
 start mongod
-start watchify -t reactify src\components\days.jsx -o public\components\days.js
-start watchify -t reactify src\components\foods.jsx -o public\components\foods.js
-start watchify -t reactify src\components\editfood.jsx -o public\components\editfood.js
+start watchify -t reactify src\components\days.jsx -o public\components\days.js -v
+start watchify -t reactify src\components\foods.jsx -o public\components\foods.js -v
+start watchify -t reactify src\components\editfood.jsx -o public\components\editfood.js -v
 start mongo meelz
